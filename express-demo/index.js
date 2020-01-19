8d1d85a9e9d664488c51d7f6924da765b4bdad41
Eugene Grechko <eugene.grechko@gmail.com>
2020-01-19 14:12:19 -0800

    Progressing with the course. Progress: Building RESTful APIs-> Route Parameters.

---
+++ express-demo/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Hello world!');
+});
+
+app.get('/api/courses', (req, res) => {
+	res.send([1, 2, 3]);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+	res.send(req.params.id);
+});
+
+const port = process.env.PORT || 3000;
+//const port = 6000;
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}...`);
+});
